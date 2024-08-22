ae173af75f7b48fd37cea5e47ac54565ea13660c
RyanKor <equus3144@gmail.com>
2024-08-22 09:12:56 +0900

    project db setup

--- imde/imde/settings.py
+++ imde/imde/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'imde.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
